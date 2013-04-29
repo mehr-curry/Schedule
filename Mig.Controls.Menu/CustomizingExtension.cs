3f5439c77e120f32d76d129728df5d885505e09d
mercury <merc@mailueberfall.de>
2013-04-29 17:05:22 +0200

    bhglv

--- Mig.Controls.Menu/CustomizingExtension.cs
+++ Mig.Controls.Menu/CustomizingExtension.cs
@@ -9,23 +9,23 @@
 using System;
 using System.Windows;
 
-namespace Mig.Controls.Menu
+namespace Mig.Controls.Customizing
 {
 	/// <summary>
 	/// Description of MenuExtensions.
 	/// </summary>
-	public class MenuExtensions
+	public class CustomizingExtension
 	{
 		public static readonly DependencyProperty KeyProperty =
-			DependencyProperty.RegisterAttached("Key", typeof(Guid?), typeof(MenuExtensions),
+			DependencyProperty.RegisterAttached("Key", typeof(Guid?), typeof(CustomizingExtension),
 			                            new FrameworkPropertyMetadata(null));
-		
-		public static Guid? GetKey(FrameworkElement element)
+
+        public static Guid? GetKey(UIElement element)
 		{
 			return (Guid?)element.GetValue(KeyProperty);
 		}
-		
-		public static void SetKey(FrameworkElement element, Guid? value)
+
+        public static void SetKey(UIElement element, Guid? value)
 		{
 			element.SetValue(KeyProperty, value);
 		}
--- ScheduleTest/MainWindow.xaml.cs
+++ ScheduleTest/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -10,10 +12,13 @@ using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
+using Mig.Controls.Customizing;
 using Mig.Controls.Schedule;
 
 namespace ScheduleTest
@@ -48,11 +53,26 @@ namespace ScheduleTest
 
         void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            
+            Dictionary<Guid, UIElement> commandingItems = new Dictionary<Guid, UIElement>();
+            CollectCommandingItems(tray.ToolBars, commandingItems);
+
         }
 
 
+        private void CollectCommandingItems(IEnumerable ic, Dictionary<Guid, UIElement> commandingItems)
+        {
+            foreach (var child in ic.OfType<UIElement>())
+            {
+                var key = CustomizingExtension.GetKey(child);
+                var childIc = child as ItemsControl;
 
+                if (key != null)
+                    commandingItems.Add(key.Value, child);
+                
+                if (childIc != null && childIc.Items.Count > 0)
+                    CollectCommandingItems(childIc.Items, commandingItems);
+            }
+        }
 
         public UiViewModel ViewModel
         {
@@ -106,6 +126,176 @@ namespace ScheduleTest
                 //    ViewModel.SelectedLes = lv.SelectedItems.Cast<Le>().ToArray();
             }
         }
+
+        private void miDatei_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void miSpeichern_Click(object sender, RoutedEventArgs e)
+        {
+
+            using (var sw = new XmlTextWriter(File.Open(@"c:\temp\toolbar_test2.xml", FileMode.Create, FileAccess.ReadWrite), Encoding.UTF8))
+            {
+                sw.Formatting = Formatting.Indented;
+                sw.Indentation = 1;
+                sw.IndentChar = '\t';
+                XamlWriter.Save(tray, sw);
+                sw.Flush();
+            }
+
+            return;
+
+            //var ms = new MemoryStream();
+            using (
+                var xmlwriter =
+                    new XmlTextWriter(
+                        File.Open(@"c:\temp\toolbar_test.xml", FileMode.Create,
+                                  FileAccess.ReadWrite), Encoding.UTF8)
+                        {
+                            Formatting = Formatting.Indented,
+                            Indentation = 1,
+                            IndentChar = '\t'
+                        })
+            {
+                xmlwriter.WriteStartDocument(true);
+                xmlwriter.WriteStartElement("toolbars_and_menus");
+                WriteCommandingItemsRecursive(tray.ToolBars, xmlwriter);
+                xmlwriter.WriteEndElement(); // "toolbars_and_menus"
+                xmlwriter.WriteEndDocument();
+
+                xmlwriter.Flush();
+            }
+            //ms.Seek(0, SeekOrigin.Begin);
+
+            //var sr = new StreamReader(ms);
+
+            //Debug.WriteLine(sr.ReadToEnd());
+        }
+
+        private void WriteCommandingItemsRecursive(IEnumerable ic, XmlWriter xw)
+        {
+            foreach (var child in ic.OfType<FrameworkElement>())
+            {
+                xw.WriteStartElement(child.GetType().Name);
+
+                var key = CustomizingExtension.GetKey(child);
+                var childIc = child as ItemsControl;
+
+                if (key != null)
+                {
+                    WriteAttribute(xw, "Key", key);
+    
+                    if(!double.IsNaN(child.Width))
+                        WriteAttribute(xw, "Width", child.Width);
+
+                    WriteAttribute(xw, "Visibility", child.Visibility);
+
+                    if (child is ButtonBase)
+                        WriteAttribute(xw, "Content", ((ButtonBase) child).Content);
+                    else if(child is MenuItem)
+                        WriteAttribute(xw, "Header", ((MenuItem)child).Header);
+                    //Debug.WriteLine(child);
+                }
+                
+                if (childIc != null && childIc.Items.Count > 0)
+                    WriteCommandingItemsRecursive(childIc.Items, xw);
+
+                xw.WriteEndElement();
+            }
+        }
+
+        private static void WriteAttribute(XmlWriter xw, string localName, object value)
+        {
+            xw.WriteStartAttribute(localName);
+            xw.WriteValue(value.ToString());
+            xw.WriteEndAttribute();
+        }
+
+        private void miOeffnen_Click(object sender, RoutedEventArgs e)
+        {
+            using (var sr = new StreamReader(File.Open(@"c:\temp\toolbar_bhpl.xaml", FileMode.Open, FileAccess.Read)))
+            {
+                Dictionary<Guid, UIElement> originals = new Dictionary<Guid, UIElement>();
+                CollectCommandingItems(tray.ToolBars, originals);
+                
+                var toolbar = (ToolBarTray)XamlReader.Parse(sr.ReadToEnd());
+                var idx = grd.Children.IndexOf(tray);
+                grd.Children.Remove(tray);
+
+                Dictionary<Guid, UIElement> customs = new Dictionary<Guid, UIElement>();
+                CollectCommandingItems(toolbar.ToolBars, customs);
+
+                bool t = originals.Keys.SequenceEqual(customs.Keys);
+
+                if (!t)
+                {
+                    AddToCustom(originals, customs);
+                    RemoveFromCustom(originals, customs);
+                }
+                tray = toolbar;
+                grd.Children.Insert(idx, tray);
+
+            }
+        }
+
+        private static void AddToCustom(Dictionary<Guid, UIElement> originals, Dictionary<Guid, UIElement> customs)
+        {
+            foreach (var addedKey in originals.Keys.Except(customs.Keys))
+            {
+                var addedElement = (FrameworkElement) originals[addedKey];
+                var parentKey = CustomizingExtension.GetKey((FrameworkElement) addedElement.Parent);
+
+                if (parentKey != null)
+                {
+                    var originalParent = originals[parentKey.Value];
+                    var customParent = customs[parentKey.Value];
+                    var originalIdx = -1;
+
+                    if (originalParent is ItemsControl)
+                        originalIdx = ((ItemsControl) originalParent).Items.IndexOf(addedElement);
+
+                    if (customParent is ItemsControl)
+                    {
+                        var clone = (UIElement)XamlReader.Parse(XamlWriter.Save(addedElement));
+                    
+                        if (originalIdx > -1)
+                            ((ItemsControl) customParent).Items.Insert(originalIdx, clone);
+                        else
+                            ((ItemsControl) customParent).Items.Add(clone);
+
+                        customs.Add(addedKey, clone);
+                    }
+                }
+            }
+        }
+
+        private static void RemoveFromCustom(Dictionary<Guid, UIElement> originals, Dictionary<Guid, UIElement> customs)
+        {
+            var removedKeys = customs.Keys.Except(originals.Keys);
+
+            for (int i = removedKeys.Count()-1; i>= 0; i--)
+            {
+                var removedKey = removedKeys.ElementAt(i);
+                var removedElement = (FrameworkElement) customs[removedKey];
+                var parentKey = CustomizingExtension.GetKey((FrameworkElement)removedElement.Parent);
+                if (parentKey != null)
+                {
+                    var customParent = customs[parentKey.Value];
+
+                    if (customParent is ItemsControl)
+                        ((ItemsControl) customParent).Items.Remove(removedElement);
+
+                    customs.Remove(removedKey);
+                }
+            }
+        }
+
+        private void miNeu_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
 
 
         

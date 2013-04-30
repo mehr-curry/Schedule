88c1cb955b8a2fccc561b7bcbb1263ee1be2eef6
mercury <merc@mailueberfall.de>
2013-04-30 16:59:17 +0200

    ka

---
+++ Mig.Controls.Menu/CommandProxy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Mig.Controls.Customizing
+{
+    public class MenuProxy
+    {
+        public FrameworkElement Owner { get; set; }
+        public Guid Key { get; set; }
+
+        public static explicit operator Menu(MenuProxy source)
+        {
+            return source.Owner.FindResource(source.Key) as Menu;
+        }
+    }
+}
